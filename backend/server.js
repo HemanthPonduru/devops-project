082e0bfcd8fd95b6f0941a7493f8d896a844c925
HemanthPonduru <hemanthponduru17@gmail.com>
2026-03-23 13:57:24 +0000

    Frontend + server setup

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,50 @@
 const express = require("express");
 const app = express();
+const pool = require("./db");
+const path = require("path");
 
+// Middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname))); // serve frontend
 
+// Home route (serves UI)
 app.get("/", (req, res) => {
-  res.send("My DevOps App is running 🚀");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
-
-// TEMPORARY in-memory storage
-let tasks = [];
 
 // Create task
-app.post("/tasks", (req, res) => {
-  const { title } = req.body;
+app.post("/tasks", async (req, res) => {
+  try {
+    console.log("Incoming:", req.body);
 
-  const newTask = {
-    id: tasks.length + 1,
-    title,
-  };
+    const { title } = req.body;
 
-  tasks.push(newTask);
+    const newTask = await pool.query(
+      "INSERT INTO tasks (title) VALUES($1) RETURNING *",
+      [title]
+    );
 
-  res.json(newTask);
+    res.json(newTask.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
-// Get tasks
-app.get("/tasks", (req, res) => {
-  res.json(tasks);
+// Get all tasks
+app.get("/tasks", async (req, res) => {
+  try {
+    const allTasks = await pool.query("SELECT * FROM tasks");
+    res.json(allTasks.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+// Start server (Render compatible)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
